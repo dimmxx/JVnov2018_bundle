e23141b126536b8960db948e2227536cf9f7dcfe
dimmxx <dimmxx@gmail.com>
2019-07-26 14:47:55 +0300

    Fri 26 Jul 14:47:55 EEST 2019

--- SandBox/src/main/java/conspect/lamba/test1/Main.java
+++ SandBox/src/main/java/conspect/lamba/test1/Main.java
@@ -69,7 +69,6 @@ OperationVariable operationVariable2 = () -> {
            return var1 + var2;
         };
 
-
         System.out.println(var1);
         System.out.println(var2);
         System.out.println(operationVariable.getResult());
