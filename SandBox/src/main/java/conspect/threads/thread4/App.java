410fc0fae4dadaab138209a524b8bce18746c4e2
dimmxx <dimmxx@gmail.com>
2019-09-10 08:43:46 +0300

    Tue Sep 10 08:43:46 EEST 2019

--- SandBox/src/main/java/conspect/threads/thread4/App.java
+++ SandBox/src/main/java/conspect/threads/thread4/App.java
@@ -1,50 +1,57 @@
 package conspect.threads.thread4;
 
+import java.util.concurrent.CountDownLatch;
+
 public class App {
 
     public static void main(String[] args) {
 
-        int[][] array = new int[10][10];
+        int[][] array = new int[100][100];
         printArray(array);
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 array[i][j] = -1;
             }
         }
 
-        array[5][5] = 0;
+        array[2][0] = 0;
         printArray(array);
 
 
+        int iteration = 0;
 
-        int itteration = 0;
+        while (iteration <= array.length * array.length) {
 
-        while (true){
+            CountDownLatch countDownLatch = new CountDownLatch(1);
+
+//            ThreadFillArray threadFillArray1 = new ThreadFillArray("forward", array, iteration, countDownLatch);
+//            Thread threadForward = new Thread(threadFillArray1);
+
+            ThreadFillArray threadFillArray2 = new ThreadFillArray("backward", array, iteration, countDownLatch);
+            Thread threadBackward = new Thread(threadFillArray2);
+
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            iteration++;
 
 
         }
 
-
-
-
-
-
-
-
+        printArray(array);
     }
-
 
     public static void printArray(int[][] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.println("|");
             for (int j = 0; j < array[i].length; j++) {
-                System.out.printf("|%2s%1s", array[i][j], "");
+                System.out.printf("|%3s%1s", array[i][j], "");
             }
         }
         System.out.println("|");
     }
-
-
 
 
 }
---
+++ SandBox/src/main/java/conspect/threads/thread4/ThreadFillArray.java
@@ -0,0 +1,68 @@
+package conspect.threads.thread4;
+
+import java.util.concurrent.CountDownLatch;
+
+public class ThreadFillArray implements Runnable {
+
+    private String direction;
+    private int[][] array;
+    private int iteration;
+    private CountDownLatch countDownLatch;
+
+    public ThreadFillArray(String direction, int[][] array, int iteration, CountDownLatch countDownLatch) {
+        this.direction = direction;
+        this.array = array;
+        this.iteration = iteration;
+        this.countDownLatch = countDownLatch;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        switch (direction) {
+            case "forward":
+                for (int i = 0; i < array.length; i++) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        writeArray(i, j, iteration);
+                    }
+                }
+                countDownLatch.countDown();
+                break;
+
+            case "backward":
+                for (int i = array.length - 1; i >= 0; i--) {
+                    for (int j = array[i].length - 1; j >= 0; j--) {
+                        writeArray(i, j, iteration);
+                    }
+                }
+                countDownLatch.countDown();
+        }
+    }
+
+    private void writeArray(int i, int j, int iteration) {
+        if (array[i][j] == iteration) {
+            try {
+                if (array[i + 1][j] == -1) array[i + 1][j] = iteration + 1;
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            }
+            try {
+                if (array[i - 1][j] == -1) array[i - 1][j] = iteration + 1;
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            }
+            try {
+                if (array[i][j + 1] == -1) array[i][j + 1] = iteration + 1;
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            }
+            try {
+                if (array[i][j - 1] == -1) array[i][j - 1] = iteration + 1;
+            } catch (ArrayIndexOutOfBoundsException e) {
+
+            }
+        }
+    }
+
+
+}
