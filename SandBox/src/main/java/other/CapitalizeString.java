a1f51cb8dee011c6a4521d252873b15e21fb1dce
dimmxx <dimmxx@gmail.com>
2019-11-07 22:08:37 +0200

    Thu  7 Nov 22:08:37 EET 2019

---
+++ SandBox/src/main/java/other/CapitalizeString.java
@@ -0,0 +1,30 @@
+package other;
+
+public class CapitalizeString {
+
+    public static void main(String[] args) {
+
+        String line = "i jvjj  c mmjd m mm m m n";
+
+    }
+
+    public static String capLine(String line, int n){
+
+        char[] array = line.toCharArray();
+
+        for(int i = 0; i < array.length; i++){
+            for (int j = n)
+            if(array[i] != ' '){
+
+            }
+
+
+        }
+
+
+        return null;
+    }
+
+
+
+}
